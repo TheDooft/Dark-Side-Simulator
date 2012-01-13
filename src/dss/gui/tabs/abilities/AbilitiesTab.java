fe82d8af59819f08c25964f3c7e1070d064a4653
Frédéric Bertolus <fred@elveos.org>
2012-01-13 17:36:45 +0100

    begin to make the ablility tab beautiful

--- src/dss/gui/tabs/abilities/AbilitiesTab.java
+++ src/dss/gui/tabs/abilities/AbilitiesTab.java
@@ -1,9 +1,15 @@
 package dss.gui.tabs.abilities;
 
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.util.LinkedList;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
 import javax.swing.TransferHandler;
 
 import dss.model.Ability;
@@ -17,14 +23,65 @@ public class AbilitiesTab extends JPanel {
 	public TransferHandler transferHandler = new AbilityTransferHandler();
 
 	public AbilitiesTab(DataModel model) {
-		setLayout(new GridLayout(1, 2));
+		setLayout(new BorderLayout());
 
 		for (Ability ablility : model.getAbilities()) {
 			availableAbilities.add(ablility);
 		}
 
-		add(new AvailableAbilityList(this));
-		add(new SelectedAbilityList(this));
+		
+		add(generateLeftPanel(), BorderLayout.WEST);
+		add(generateRightPanel(), BorderLayout.CENTER);
+		
+		
 
+	}
+	
+	private JPanel generateLeftPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(10,20,20,20));
+		
+		
+		JLabel title = new JLabel("Available abilities");
+		title.setFont(title.getFont().deriveFont(16f));
+		title.setFont(title.getFont().deriveFont(title.getFont().getStyle() ^ Font.BOLD));
+		panel.add(title, BorderLayout.NORTH);
+		
+		
+		
+		AvailableAbilityList availableAbilityList = new AvailableAbilityList(this);
+		
+		panel.add(new JScrollPane(availableAbilityList), BorderLayout.CENTER);
+
+		return panel;
+	}
+	
+	private JPanel generateRightPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(10,20,20,20));
+		
+		JLabel title = new JLabel("Selected abilities");
+		title.setFont(title.getFont().deriveFont(16f));
+		title.setFont(title.getFont().deriveFont(title.getFont().getStyle() ^ Font.BOLD));
+		panel.add(title, BorderLayout.NORTH);
+		
+		SelectedAbilityList selectedAbilityList = new SelectedAbilityList(this);
+		panel.add(new JScrollPane(selectedAbilityList), BorderLayout.CENTER);
+		
+		panel.add(generateDetailPanel(), BorderLayout.SOUTH);
+		
+		return panel;
+	}
+	
+	private JPanel generateDetailPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createEmptyBorder(20,0,0,0));
+		
+		panel.setMinimumSize(new Dimension(0, 100));
+		
+		return panel;
 	}
 }
